658d68fc9efb4fdc2123f31af33e29b6f3ea6cd5
jeannethesali03 <eyleen.salinas@catolica.edu.sv>
2025-02-24 17:08:59 -0600

    Agregar archivos de proyecto.

---
+++ P01_2022SH651-2022RC650/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace P01_2022SH651_2022RC650
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
